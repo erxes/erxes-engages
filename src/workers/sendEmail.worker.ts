710ae81ff227d79e64e3d20d05fd6208c4f04f6c
Buyantogtokh <buyantogtokh.s@nmma.co>
2019-11-30 14:36:45 +0800

    send userId in unsubsribe url

--- src/workers/sendEmail.worker.ts
+++ src/workers/sendEmail.worker.ts
@@ -46,7 +46,7 @@ connect().then(async () => {
 
     const MAIN_API_DOMAIN = getEnv({ name: 'MAIN_API_DOMAIN' });
 
-    const unSubscribeUrl = `${MAIN_API_DOMAIN}/unsubscribe/?cid=${customer._id}`;
+    const unSubscribeUrl = `${MAIN_API_DOMAIN}/unsubscribe/?cid=${customer._id}&uid=${user._id}`;
 
     replacedContent += `<div style="padding: 10px; color: #ccc; text-align: center; font-size:12px;">If you want to use service like this click <a style="text-decoration: underline; color: #ccc;" href="https://erxes.io" target="_blank">here</a> to read more. Also you can opt out from our email subscription <a style="text-decoration: underline;color: #ccc;" rel="noopener" target="_blank" href="${unSubscribeUrl}">here</a>.  <br>© 2019 erxes inc Growth Marketing Platform </div>`;
 
