d3e275cdfcec68cdffdfd4d23eb02d6c932760d2
BatAmar Battulga <batamar.b@nmtec.co>
2019-12-28 20:19:06 +0800

    remove unused code

--- src/debuggers.ts
+++ src/debuggers.ts
@@ -4,9 +4,7 @@ export const debugInit = debug('erxes-engages:init');
 export const debugDb = debug('erxes-engages:db');
 export const debugBase = debug('erxes-engages:base');
 export const debugEngages = debug('erxes-engages:engages');
-export const debugExternalRequests = debug('erxes-engages:external-requests');
 export const debugWorkers = debug('erxes-engages:worker');
-export const debugExternalApi = debug('erxes-engages:external-api-fetcher');
 
 export const debugRequest = (debugInstance, req) =>
   debugInstance(`
@@ -14,6 +12,3 @@ export const debugRequest = (debugInstance, req) =>
         body: ${JSON.stringify(req.body || {})}
         queryParams: ${JSON.stringify(req.query)}
     `);
-
-export const debugResponse = (debugInstance, req, data = 'success') =>
-  debugInstance(`Responding ${req.path} request to ${req.headers.origin} with ${data}`);
