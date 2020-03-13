235b1424a2f53b13c6b530fba3a6dd5de30ef2c8
munkhjin <munkhjin0223@gmail.com>
2020-03-13 14:14:56 +0800

    add `badsyntax` status for email verification status

--- src/models/Emails.ts
+++ src/models/Emails.ts
@@ -7,7 +7,8 @@ export const EMAIL_VALIDATION_STATUSES = {
   UNKNOWN: 'unknown',
   DISPOSABLE: 'disposable',
   CATCHALL: 'catchall',
-  ALL: ['valid', 'invalid', 'accept_all_unverifiable', 'unknown', 'disposable', 'catchall'],
+  BAD_SYNTAX: 'badsyntax',
+  ALL: ['valid', 'invalid', 'accept_all_unverifiable', 'unknown', 'disposable', 'catchall', 'badsyntax'],
 };
 
 interface IEmail {
